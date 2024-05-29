2758171b56bc600853cc26ab3b01127cc3cc46b3
Krish Patel <patel.krish1003@gmail.com>
2024-05-30 00:41:14 +0530

    changed chunker

--- app/chunker.py
+++ app/chunker.py
@@ -1,4 +1,24 @@
-def chunk_text(text, chunk_size=100):
-    words = text.split()
-    chunks = [" ".join(words[i:i + chunk_size]) for i in range(0, len(words), chunk_size)]
+import tiktoken
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+tokenizer = tiktoken.get_encoding('cl100k_base')
+
+# create the length function
+def tiktoken_len(text):
+    tokens = tokenizer.encode(
+        text,
+        disallowed_special=()
+    )
+    return len(tokens)
+
+def chunk_text(text):
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=400,
+        chunk_overlap=20,  # number of tokens overlap between chunks
+        length_function=tiktoken_len,
+        separators=['\n\n', '\n', ' ', '']
+    )
+
+    chunks = text_splitter.split_text(text)
     return chunks
